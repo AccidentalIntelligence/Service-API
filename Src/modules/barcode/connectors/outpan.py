3717b1c48adb952398210ddfc2c4aff2ceb9c8a3
Dave Belcher <ukbelch@gmail.com>
2015-03-15 17:55:16 +0000

    Added API key for barcode scanning
    Added the API key.

--- Src/modules/barcode/connectors/outpan.py
+++ Src/modules/barcode/connectors/outpan.py
@@ -7,6 +7,7 @@ import types
 
 # Service Configuration
 api_url = "http://www.outpan.com/api/get_product.php?"
+api_key = "008ec8d190294efc3cd0f7188f836b2a"
 
 def getType(data):
     if 'Manufacturer' in data:
@@ -18,37 +19,38 @@ def getType(data):
     return 0
 
 def getBarcodeData(code):
-    global key, format, num_days, include_location, api_url
-    
+    global api_key, format, num_days, include_location, api_url
+
     logging.info("Retrieving barcode data from outpan")
     logging.info("Using code: "+code)
-    
+
     barcodeData = {}
     barcodeData['code_id'] = code
-    
+
     # Grab data for location
     data = dict()
     data['barcode'] = code
-    
+    data['apikey'] = api_key
+
     http = httplib2.Http()
-    
+
     response, content = http.request(api_url+urlencode(data))
-    
+
     print content;
-    
+
     error = False
-    
+
     if 'error' in content:
         error = True
-    
+
     if error:
         barcodeData['type'] = 0
         return barcodeData
-        
+
     print "no error!"
     data = json.loads(content)['attributes']
     print data
-    
+
     barcodeData['type'] = getType(data);
-        
+
     return barcodeData
