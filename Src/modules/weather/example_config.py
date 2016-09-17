64f48491b7a114953dd5c3033d0faceb562bdd83
Dave Belcher <ukbelch@gmail.com>
2016-09-17 11:22:43 -0700

    Cleanup of the older API modules

---
+++ Src/modules/weather/example_config.py
@@ -0,0 +1,15 @@
+# to use, remove the example_ from the filename and provide
+# all of the values in <>.
+
+# Database config
+dbhost = "<db host>"
+dbuser = "<db user>"
+dbpass = "<db pass>"
+dbname = "<barcode_service>"
+
+# WorldWeatherOnline API
+search_url = "http://api.worldweatheronline.com/free/v1/search.ashx?"
+api_url = "http://www.outpan.com/api/get_product.php?"
+api_key = "<api key>"
+num_days = "<days to collect>"
+include_location = "<yes or no>"
