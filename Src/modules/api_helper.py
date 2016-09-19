a8f5d781bbeec88239ab03042ec0840bcc3670ec
Dave Belcher <ukbelch@gmail.com>
2016-09-18 22:41:06 -0700

    We will get this eventually...

--- Src/modules/api_helper.py
+++ Src/modules/api_helper.py
@@ -8,12 +8,7 @@ def register_api(api):
         # do pre-wrap
         print "Registering API: " + api
         def wrapped(query):
-            global has_config
-            print "Wrapped has_config: " + has_config
-            if has_config:
-                return func(query)
-            else:
-                return '{"error":"Configuration not loaded for API: '+api+'"}'
+            return func(query)
         wrapped.func_name = func.func_name
         api_register[api] = wrapped
         return wrapped
