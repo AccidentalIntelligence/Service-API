c5f5d1bf9bf930b6c150665aea658c3f215ec59a
Dave Belcher <ukbelch@gmail.com>
2016-09-18 23:38:43 -0700

    New, dynamic module loading... testing

--- Src/modules/__init__.py
+++ Src/modules/__init__.py
@@ -2,4 +2,4 @@ import os
 import glob
 modules = glob.glob(os.path.dirname(__file__)+"/*/api.py")
 print modules
-__all__ = [ os.path.basename(f)[:-3] for f in modules]
+__all__ = [".".join(f[:-3].split('/')[-2:]) for f in modules]
