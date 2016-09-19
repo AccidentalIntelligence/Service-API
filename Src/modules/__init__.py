e4fe961f6b0f5dcd2f358e2700463a1f92d5a8da
Dave Belcher <ukbelch@gmail.com>
2016-09-19 00:17:53 -0700

    Trying something else

--- Src/modules/__init__.py
+++ Src/modules/__init__.py
@@ -1,9 +1,7 @@
 import os
 import glob
 modules = glob.glob(os.path.dirname(__file__)+"/*/api.py")
-print modules
 modules = [".".join(f[:-3].split('/')[-2:]) for f in modules]
-for mod in modules:
-    import mod.api as mod
 print modules
-__all__ = modules
+for mod in modules:
+    __import__(mod).api
