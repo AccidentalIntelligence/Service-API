9a6938b3502cb0fb75a473e2522e7d7e1e1a9192
Capn Flint <capnflinttv@gmail.com>
2019-09-25 13:51:06 -0700

    better mapping of certificates

--- Src/start.sh
+++ Src/start.sh
@@ -1,2 +1,2 @@
 sudo docker build -t serviceapi .
-sudo docker run -dit --restart unless-stopped  -p 4443:4443 -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock serviceapi
+sudo docker run --rm -dit --restart unless-stopped  -p 4443:4443 -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock -v $(sudo readlink -f /etc/letsencrypt/live/www.capnflint.com/privkey.pem):/app/certs/privkey.pem:ro -v $(sudo readlink -f /etc/letsencrypt/live/www.capnflint.com/fullchain.pem):/app/certs/fullchain.pem:ro --name serviceapi serviceapi
