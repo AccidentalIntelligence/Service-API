45a895f86356dd5031d7c7516cdad3f9ac524ba3
Capn Flint <capnflinttv@gmail.com>
2019-11-10 22:49:15 -0800

    Update rsi.py

--- Src/modules/rsi/connectors/rsi.py
+++ Src/modules/rsi/connectors/rsi.py
@@ -77,7 +77,7 @@ def bs_parse_citizen(html, baseurl):
             for d in div.select('div'):
                 if 'profile-content' in d.get('class'):
                     print(d)
-                    parsed['CitizenNo'] = d.p.string.text
+                    parsed['CitizenNo'] = d.p.strong.text
                 
                 if 'thumb' in d.get('class'):
                     parsed['logo'] = baseurl + d.img['src']
